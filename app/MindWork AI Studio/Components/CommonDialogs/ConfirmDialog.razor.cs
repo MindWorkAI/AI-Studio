407066a5a0c912ad953f03005959cbc105900da2
Thorsten Sommer <mail@tsommer.org>
2024-04-20 17:07:27 +0200

    Added confirmation dialog

---
+++ app/MindWork AI Studio/Components/CommonDialogs/ConfirmDialog.razor.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components;
+
+using MudBlazor;
+
+namespace AIStudio.Components.CommonDialogs;
+
+public partial class ConfirmDialog : ComponentBase
+{
+    [CascadingParameter]
+    private MudDialogInstance MudDialog { get; set; } = null!;
+
+    [Parameter]
+    public string Message { get; set; } = string.Empty;
+
+    private void Cancel() => this.MudDialog.Cancel();
+    
+    private void Confirm() => this.MudDialog.Close(DialogResult.Ok(true));
+}
