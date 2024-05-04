36fb450fad73b2dcb7fbf1f30440fb41d49b8191
Thorsten Sommer <mail@tsommer.org>
2024-05-04 10:58:32 +0200

    Added data structure for models

---
+++ app/MindWork AI Studio/Provider/Model.cs
@@ -0,0 +1,7 @@
+namespace AIStudio.Provider;
+
+/// <summary>
+/// The data model for the model to use.
+/// </summary>
+/// <param name="Id">The model's ID.</param>
+public readonly record struct Model(string Id);
