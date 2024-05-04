bbb9d883a1192afc129dc40ed0f60a89d8f80be9
Thorsten Sommer <mail@tsommer.org>
2024-05-04 10:58:18 +0200

    Refined provider interface

--- app/MindWork AI Studio/Provider/IProvider.cs
+++ app/MindWork AI Studio/Provider/IProvider.cs
@@ -1,5 +1,8 @@
+using AIStudio.Chat;
 using AIStudio.Settings;
 using Microsoft.JSInterop;
+
+using MudBlazor;
 
 namespace AIStudio.Provider;
 
@@ -19,7 +22,45 @@ public interface IProvider
     /// </summary>
     public string InstanceName { get; set; }
     
-    public IAsyncEnumerable<string> GetChatCompletion(IJSRuntime jsRuntime, SettingsManager settings, Model chatModel, Thread chatThread);
+    /// <summary>
+    /// Starts a chat completion stream.
+    /// </summary>
+    /// <param name="jsRuntime">The JS runtime to access the Rust code.</param>
+    /// <param name="settings">The settings manager to access the API key.</param>
+    /// <param name="chatModel">The model to use for chat completion.</param>
+    /// <param name="chatThread">The chat thread to continue.</param>
+    /// <param name="token">The cancellation token.</param>
+    /// <returns>The chat completion stream.</returns>
+    public IAsyncEnumerable<string> StreamChatCompletion(IJSRuntime jsRuntime, SettingsManager settings, Model chatModel, ChatThread chatThread, CancellationToken token = default);
     
-    public Task<IList<Model>> GetModels(IJSRuntime jsRuntime, SettingsManager settings);
+    /// <summary>
+    /// Starts an image completion stream.
+    /// </summary>
+    /// <param name="jsRuntime">The JS runtime to access the Rust code.</param>
+    /// <param name="settings">The settings manager to access the API key.</param>
+    /// <param name="imageModel">The model to use for image completion.</param>
+    /// <param name="promptPositive">The positive prompt.</param>
+    /// <param name="promptNegative">The negative prompt.</param>
+    /// <param name="referenceImageURL">The reference image URL.</param>
+    /// <param name="token">The cancellation token.</param>
+    /// <returns>The image completion stream.</returns>
+    public IAsyncEnumerable<ImageURL> StreamImageCompletion(IJSRuntime jsRuntime, SettingsManager settings, Model imageModel, string promptPositive, string promptNegative = FilterOperator.String.Empty, ImageURL referenceImageURL = default, CancellationToken token = default);
+    
+    /// <summary>
+    /// Load all possible text models that can be used with this provider.
+    /// </summary>
+    /// <param name="jsRuntime">The JS runtime to access the Rust code.</param>
+    /// <param name="settings">The settings manager to access the API key.</param>
+    /// <param name="token">The cancellation token.</param>
+    /// <returns>The list of text models.</returns>
+    public Task<IList<Model>> GetTextModels(IJSRuntime jsRuntime, SettingsManager settings, CancellationToken token = default);
+    
+    /// <summary>
+    /// Load all possible image models that can be used with this provider.
+    /// </summary>
+    /// <param name="jsRuntime">The JS runtime to access the Rust code.</param>
+    /// <param name="settings">The settings manager to access the API key.</param>
+    /// <param name="token">The cancellation token.</param>
+    /// <returns>The list of image models.</returns>
+    public Task<IList<Model>> GetImageModels(IJSRuntime jsRuntime, SettingsManager settings, CancellationToken token = default);
 }
