f11b9e02996de9f28f837e62ed961192db205a37
Thorsten Sommer <mail@tsommer.org>
2024-05-04 10:41:14 +0200

    Add configuration for snackbar

--- app/MindWork AI Studio/Program.cs
+++ app/MindWork AI Studio/Program.cs
@@ -5,7 +5,18 @@ using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 6_000; //milliseconds aka 6 seconds
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Outlined;
+});
+
 builder.Services.AddMudMarkdownServices();
 builder.Services.AddSingleton<SettingsManager>();
 builder.Services.AddRazorComponents()
