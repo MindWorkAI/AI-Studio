0541e1b18ec2ffdb1e72fd10d969f39dedb8b85d
Thorsten Sommer <mail@tsommer.org>
2024-05-04 11:00:46 +0200

    Added options for energy saving & send shortcuts

--- app/MindWork AI Studio/Settings/Data.cs
+++ app/MindWork AI Studio/Settings/Data.cs
@@ -15,4 +15,15 @@ public sealed class Data
     /// List of configured providers.
     /// </summary>
     public List<Provider> Providers { get; init; } = new();
+
+    /// <summary>
+    /// Should we save energy? When true, we will update content streamed
+    /// from the server, i.e., AI, less frequently.
+    /// </summary>
+    public bool IsSavingEnergy { get; set; }
+
+    /// <summary>
+    /// Shortcuts to send the input to the AI.
+    /// </summary>
+    public SendBehavior ShortcutSendBehavior { get; set; } = SendBehavior.MODIFER_ENTER_IS_SENDING;
 }
---
+++ app/MindWork AI Studio/Settings/SendBehavior.cs
@@ -0,0 +1,28 @@
+namespace AIStudio.Settings;
+
+/// <summary>
+/// Possible behaviors for sending the input to the AI.
+/// </summary>
+public enum SendBehavior
+{
+    /// <summary>
+    /// There is no shortcut to send the input to the AI. The user must click
+    /// the send button. The enter key adds a new line.
+    /// </summary>
+    NO_KEY_IS_SENDING,
+    
+    /// <summary>
+    /// The user can send the input to the AI by pressing any modifier key
+    /// together with the enter key. Alternatively, the user can click the send
+    /// button. The enter key alone adds a new line.
+    /// </summary>
+    MODIFER_ENTER_IS_SENDING,
+    
+    /// <summary>
+    /// The user can send the input to the AI by pressing the enter key. In order
+    /// to add a new line, the user must press the shift key together with the
+    /// enter key. Alternatively, the user can click the send button to send the
+    /// input to the AI.
+    /// </summary>
+    ENTER_IS_SENDING,
+}
