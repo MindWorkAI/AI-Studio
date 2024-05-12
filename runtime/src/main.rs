c633053663eb72516078526389dc8756dbe43652
Thorsten Sommer <mail@tsommer.org>
2024-05-12 14:34:29 +0200

    Fixed spelling

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,4 +1,4 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// Prevents an additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use arboard::Clipboard;
