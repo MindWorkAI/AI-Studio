df8f207e7fa008e3ce29471c878b8d2c19f6222d
Thorsten Sommer <mail@tsommer.org>
2024-06-02 20:52:30 +0200

    Added changelog component

---
+++ app/MindWork AI Studio/Components/Blocks/Changelog.razor.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components;
+
+namespace AIStudio.Components.Blocks;
+
+public partial class Changelog : ComponentBase
+{
+    [Inject]
+    private HttpClient HttpClient { get; set; } = null!;
+
+    #region Overrides of ComponentBase
+
+    protected override async Task OnInitializedAsync()
+    {
+        await this.ReadLogAsync();
+        await base.OnInitializedAsync();
+    }
+
+    #endregion
+
+    private Log SelectedLog { get; set; } = LOGS.MaxBy(n => n.Build);
+    
+    private string LogContent { get; set; } = string.Empty;
+
+    private async Task ReadLogAsync()
+    {
+        var response = await this.HttpClient.GetAsync($"changelog/{this.SelectedLog.Filename}");
+        this.LogContent = await response.Content.ReadAsStringAsync();
+    }
+}
