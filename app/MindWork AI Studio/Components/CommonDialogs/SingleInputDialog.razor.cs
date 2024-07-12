bf83c1830b3163b9ed3d5ca8d7a9da5b234245cc
Thorsten Sommer <mail@tsommer.org>
2024-07-12 16:04:47 +0200

    Allow customization of confirmation button color

--- app/MindWork AI Studio/Components/CommonDialogs/SingleInputDialog.razor.cs
+++ app/MindWork AI Studio/Components/CommonDialogs/SingleInputDialog.razor.cs
@@ -16,6 +16,9 @@ public partial class SingleInputDialog : ComponentBase
     [Parameter]
     public string ConfirmText { get; set; } = "OK";
 
+    [Parameter]
+    public Color ConfirmColor { get; set; } = Color.Error;
+
     private void Cancel() => this.MudDialog.Cancel();
     
     private void Confirm() => this.MudDialog.Close(DialogResult.Ok(this.UserInput));
