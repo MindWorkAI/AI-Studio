4554cde08577c0929f22a3defb1336ddd74f85b3
Thorsten Sommer <mail@tsommer.org>
2024-07-14 20:34:16 +0200

    Added text summarization assistant

---
+++ app/MindWork AI Studio/Tools/CommonLanguages.cs
@@ -0,0 +1,19 @@
+namespace AIStudio.Tools;
+
+public enum CommonLanguages
+{
+    AS_IS,
+    
+    EN_US,
+    EN_GB,
+    ZH_CN,
+    HI_IN,
+    ES_ES,
+    FR_FR,
+    DE_DE,
+    DE_CH,
+    DE_AT,
+    JA_JP,
+    
+    OTHER,
+}
