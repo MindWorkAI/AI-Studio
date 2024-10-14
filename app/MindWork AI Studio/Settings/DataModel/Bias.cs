3962c5794f304e73d22a424763eed861526739f9
Thorsten Sommer <mail@tsommer.org>
2024-10-14 19:34:06 +0200

    Added bias data classes

---
+++ app/MindWork AI Studio/Settings/DataModel/Bias.cs
@@ -0,0 +1,29 @@
+namespace AIStudio.Settings.DataModel;
+
+public sealed class Bias
+{
+    /// <summary>
+    /// The unique identifier of the bias.
+    /// </summary>
+    public Guid Id { get; init; } = Guid.Empty;
+
+    /// <summary>
+    /// In which category the bias is located.
+    /// </summary>
+    public BiasCategory Category { get; set; } = BiasCategory.NONE;
+    
+    /// <summary>
+    /// The bias description.
+    /// </summary>
+    public string Description { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Related bias.
+    /// </summary>
+    public IReadOnlyList<Guid> Related { get; init; } = [];
+
+    /// <summary>
+    /// Related links.
+    /// </summary>
+    public IReadOnlyList<string> Links { get; init; } = [];
+}
