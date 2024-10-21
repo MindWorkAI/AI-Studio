0b367a4542e49819d3ada479226bcf043d888716
Thorsten Sommer <mail@tsommer.org>
2024-10-21 18:42:10 +0200

    Add method to get random bias

--- app/MindWork AI Studio/Settings/DataModel/BiasCatalog.cs
+++ app/MindWork AI Studio/Settings/DataModel/BiasCatalog.cs
@@ -5909,4 +5909,23 @@ public static class BiasCatalog
         { IMPLICIT_STEREOTYPES.Id, IMPLICIT_STEREOTYPES },
         { IMPLICIT_ASSOCIATIONS.Id, IMPLICIT_ASSOCIATIONS },
     };
+    
+    public static Bias GetRandomBias(ref IList<int> usedBias)
+    {
+        if(usedBias.Count >= ALL_BIAS.Count)
+            usedBias.Clear();
+        
+        int randomBiasIndex;
+        lock (RANDOM)
+        {
+            randomBiasIndex = RANDOM.Next(0, ALL_BIAS.Count);
+            while(usedBias.Contains(randomBiasIndex))
+                randomBiasIndex = RANDOM.Next(0, ALL_BIAS.Count);
+        }
+        
+        usedBias.Add(randomBiasIndex);
+        return ALL_BIAS.Values.ElementAt(randomBiasIndex);
+    }
+    
+    private static readonly Random RANDOM = new();
 }
