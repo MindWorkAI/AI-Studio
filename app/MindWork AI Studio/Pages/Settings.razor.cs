53b416de2debc33ad5e3734e880f8088d349790b
Thorsten Sommer <mail@tsommer.org>
2025-01-05 14:49:25 +0100

    Refactored provider settings

--- app/MindWork AI Studio/Pages/Settings.razor.cs
+++ app/MindWork AI Studio/Pages/Settings.razor.cs
@@ -1,11 +1,6 @@
-using AIStudio.Dialogs;
-using AIStudio.Provider;
 using AIStudio.Settings;
 
 using Microsoft.AspNetCore.Components;
-
-using DialogOptions = AIStudio.Dialogs.DialogOptions;
-using RustService = AIStudio.Tools.RustService;
 
 // ReSharper disable ClassNeverInstantiated.Global
 
@@ -14,18 +9,9 @@ namespace AIStudio.Pages;
 public partial class Settings : ComponentBase, IMessageBusReceiver, IDisposable
 {
     [Inject]
-    private SettingsManager SettingsManager { get; init; } = null!;
-
-    [Inject]
-    private IDialogService DialogService { get; init; } = null!;
-    
-    [Inject]
     private MessageBus MessageBus { get; init; } = null!;
     
-    [Inject]
-    private RustService RustService { get; init; } = null!;
-    
-    private readonly List<ConfigurationSelectData<string>> availableLLMProviders = new();
+    private List<ConfigurationSelectData<string>> availableLLMProviders = new();
     private List<ConfigurationSelectData<string>> availableEmbeddingProviders = new();
 
     #region Overrides of ComponentBase
@@ -36,127 +22,7 @@ public partial class Settings : ComponentBase, IMessageBusReceiver, IDisposable
         this.MessageBus.RegisterComponent(this);
         this.MessageBus.ApplyFilters(this, [], [ Event.CONFIGURATION_CHANGED ]);
         
-        this.UpdateProviders();
         await base.OnInitializedAsync();
-    }
-
-    #endregion
-    
-    #region Provider related
-
-    private async Task AddLLMProvider()
-    {
-        var dialogParameters = new DialogParameters<ProviderDialog>
-        {
-            { x => x.IsEditing, false },
-        };
-        
-        var dialogReference = await this.DialogService.ShowAsync<ProviderDialog>("Add LLM Provider", dialogParameters, DialogOptions.FULLSCREEN);
-        var dialogResult = await dialogReference.Result;
-        if (dialogResult is null || dialogResult.Canceled)
-            return;
-
-        var addedProvider = (AIStudio.Settings.Provider)dialogResult.Data!;
-        addedProvider = addedProvider with { Num = this.SettingsManager.ConfigurationData.NextProviderNum++ };
-        
-        this.SettingsManager.ConfigurationData.Providers.Add(addedProvider);
-        this.UpdateProviders();
-        
-        await this.SettingsManager.StoreSettings();
-        await this.MessageBus.SendMessage<bool>(this, Event.CONFIGURATION_CHANGED);
-    }
-
-    private async Task EditLLMProvider(AIStudio.Settings.Provider provider)
-    {
-        var dialogParameters = new DialogParameters<ProviderDialog>
-        {
-            { x => x.DataNum, provider.Num },
-            { x => x.DataId, provider.Id },
-            { x => x.DataInstanceName, provider.InstanceName },
-            { x => x.DataLLMProvider, provider.UsedLLMProvider },
-            { x => x.DataModel, provider.Model },
-            { x => x.DataHostname, provider.Hostname },
-            { x => x.IsSelfHosted, provider.IsSelfHosted },
-            { x => x.IsEditing, true },
-            { x => x.DataHost, provider.Host },
-        };
-
-        var dialogReference = await this.DialogService.ShowAsync<ProviderDialog>("Edit LLM Provider", dialogParameters, DialogOptions.FULLSCREEN);
-        var dialogResult = await dialogReference.Result;
-        if (dialogResult is null || dialogResult.Canceled)
-            return;
-
-        var editedProvider = (AIStudio.Settings.Provider)dialogResult.Data!;
-        
-        // Set the provider number if it's not set. This is important for providers
-        // added before we started saving the provider number.
-        if(editedProvider.Num == 0)
-            editedProvider = editedProvider with { Num = this.SettingsManager.ConfigurationData.NextProviderNum++ };
-        
-        this.SettingsManager.ConfigurationData.Providers[this.SettingsManager.ConfigurationData.Providers.IndexOf(provider)] = editedProvider;
-        this.UpdateProviders();
-        
-        await this.SettingsManager.StoreSettings();
-        await this.MessageBus.SendMessage<bool>(this, Event.CONFIGURATION_CHANGED);
-    }
-
-    private async Task DeleteLLMProvider(AIStudio.Settings.Provider provider)
-    {
-        var dialogParameters = new DialogParameters
-        {
-            { "Message", $"Are you sure you want to delete the provider '{provider.InstanceName}'?" },
-        };
-        
-        var dialogReference = await this.DialogService.ShowAsync<ConfirmDialog>("Delete LLM Provider", dialogParameters, DialogOptions.FULLSCREEN);
-        var dialogResult = await dialogReference.Result;
-        if (dialogResult is null || dialogResult.Canceled)
-            return;
-        
-        var deleteSecretResponse = await this.RustService.DeleteAPIKey(provider);
-        if(deleteSecretResponse.Success)
-        {
-            this.SettingsManager.ConfigurationData.Providers.Remove(provider);
-            await this.SettingsManager.StoreSettings();
-        }
-        
-        this.UpdateProviders();
-        await this.MessageBus.SendMessage<bool>(this, Event.CONFIGURATION_CHANGED);
-    }
-
-    private string GetLLMProviderModelName(AIStudio.Settings.Provider provider)
-    {
-        const int MAX_LENGTH = 36;
-        var modelName = provider.Model.ToString();
-        return modelName.Length > MAX_LENGTH ? "[...] " + modelName[^Math.Min(MAX_LENGTH, modelName.Length)..] : modelName;
-    }
-    
-    private void UpdateProviders()
-    {
-        this.availableLLMProviders.Clear();
-        foreach (var provider in this.SettingsManager.ConfigurationData.Providers)
-            this.availableLLMProviders.Add(new (provider.InstanceName, provider.Id));
-    }
-
-    private string GetCurrentConfidenceLevelName(LLMProviders llmProvider)
-    {
-        if (this.SettingsManager.ConfigurationData.LLMProviders.CustomConfidenceScheme.TryGetValue(llmProvider, out var level))
-            return level.GetName();
-
-        return "Not yet configured";
-    }
-    
-    private string SetCurrentConfidenceLevelColorStyle(LLMProviders llmProvider)
-    {
-        if (this.SettingsManager.ConfigurationData.LLMProviders.CustomConfidenceScheme.TryGetValue(llmProvider, out var level))
-            return $"background-color: {level.GetColor(this.SettingsManager)};";
-
-        return $"background-color: {ConfidenceLevel.UNKNOWN.GetColor(this.SettingsManager)};";
-    }
-
-    private async Task ChangeCustomConfidenceLevel(LLMProviders llmProvider, ConfidenceLevel level)
-    {
-        this.SettingsManager.ConfigurationData.LLMProviders.CustomConfidenceScheme[llmProvider] = level;
-        await this.SettingsManager.StoreSettings();
     }
 
     #endregion
