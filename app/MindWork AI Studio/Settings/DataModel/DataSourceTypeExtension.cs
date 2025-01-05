c62bcb11f41bc840b7dd290446c010ada11a9c64
Thorsten Sommer <mail@tsommer.org>
2025-01-05 16:14:41 +0100

    Added documentation to data source types

--- app/MindWork AI Studio/Settings/DataModel/DataSourceTypeExtension.cs
+++ app/MindWork AI Studio/Settings/DataModel/DataSourceTypeExtension.cs
@@ -1,7 +1,15 @@
 namespace AIStudio.Settings.DataModel;
 
+/// <summary>
+/// Extension methods for data source types.
+/// </summary>
 public static class DataSourceTypeExtension
 {
+    /// <summary>
+    /// Get the display name of the data source type.
+    /// </summary>
+    /// <param name="type">The data source type.</param>
+    /// <returns>The display name of the data source type.</returns>
     public static string GetDisplayName(this DataSourceType type)
     {
         return type switch
