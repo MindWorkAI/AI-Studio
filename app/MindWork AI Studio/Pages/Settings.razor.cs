29a5798b830791246a9280268916fcf4a4d8b713
Thorsten Sommer <mail@tsommer.org>
2025-01-05 13:18:00 +0100

    Factored bias-of-the-day settings

--- app/MindWork AI Studio/Pages/Settings.razor.cs
+++ app/MindWork AI Studio/Pages/Settings.razor.cs
@@ -335,29 +335,6 @@ public partial class Settings : ComponentBase, IMessageBusReceiver, IDisposable
     }
 
     #endregion
-
-    #region Bias-of-the-day related
-
-    private async Task ResetBiasOfTheDayHistory()
-    {
-        var dialogParameters = new DialogParameters
-        {
-            { "Message", "Are you sure you want to reset your bias-of-the-day statistics? The system will no longer remember which biases you already know. As a result, biases you are already familiar with may be addressed again." },
-        };
-        
-        var dialogReference = await this.DialogService.ShowAsync<ConfirmDialog>("Reset your bias-of-the-day statistics", dialogParameters, DialogOptions.FULLSCREEN);
-        var dialogResult = await dialogReference.Result;
-        if (dialogResult is null || dialogResult.Canceled)
-            return;
-        
-        this.SettingsManager.ConfigurationData.BiasOfTheDay.UsedBias.Clear();
-        this.SettingsManager.ConfigurationData.BiasOfTheDay.DateLastBiasDrawn = DateOnly.MinValue;
-        await this.SettingsManager.StoreSettings();
-        
-        await this.MessageBus.SendMessage<bool>(this, Event.CONFIGURATION_CHANGED);
-    }
-
-    #endregion
     
     #region Implementation of IMessageBusReceiver
 
