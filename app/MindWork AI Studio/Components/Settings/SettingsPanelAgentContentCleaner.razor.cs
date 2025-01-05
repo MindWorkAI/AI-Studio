c77753f1f5cdcc946544c100c5d818f750427749
Thorsten Sommer <mail@tsommer.org>
2025-01-05 13:17:28 +0100

    Introduced a base class for settings panels

--- app/MindWork AI Studio/Components/Settings/SettingsPanelAgentContentCleaner.razor.cs
+++ app/MindWork AI Studio/Components/Settings/SettingsPanelAgentContentCleaner.razor.cs
@@ -1,14 +1,3 @@
-using AIStudio.Settings;
-
-using Microsoft.AspNetCore.Components;
-
 namespace AIStudio.Components.Settings;
 
-public partial class SettingsPanelAgentContentCleaner : ComponentBase
-{
-    [Parameter]
-    public Func<IReadOnlyList<ConfigurationSelectData<string>>> AvailableLLMProvidersFunc { get; set; } = () => [];
-    
-    [Inject]
-    private SettingsManager SettingsManager { get; init; } = null!;
-}
+public partial class SettingsPanelAgentContentCleaner : SettingsPanelBase;
---
+++ app/MindWork AI Studio/Components/Settings/SettingsPanelBase.cs
@@ -0,0 +1,20 @@
+using AIStudio.Settings;
+
+using Microsoft.AspNetCore.Components;
+
+namespace AIStudio.Components.Settings;
+
+public abstract class SettingsPanelBase : ComponentBase
+{
+    [Parameter]
+    public Func<IReadOnlyList<ConfigurationSelectData<string>>> AvailableLLMProvidersFunc { get; set; } = () => [];
+    
+    [Inject]
+    protected SettingsManager SettingsManager { get; init; } = null!;
+    
+    [Inject]
+    protected IDialogService DialogService { get; init; } = null!;
+    
+    [Inject]
+    protected MessageBus MessageBus { get; init; } = null!;
+}
