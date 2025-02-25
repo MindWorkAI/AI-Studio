5a8b5154c9fe0f13de16fb3ff0f1ee446eec1048
Thorsten Sommer <mail@tsommer.org>
2025-02-25 19:19:46 +0100

    Applied code fix

--- app/MindWork AI Studio/Settings/DataModel/ThemesExtensions.cs
+++ app/MindWork AI Studio/Settings/DataModel/ThemesExtensions.cs
@@ -2,15 +2,12 @@ namespace AIStudio.Settings.DataModel;
 
 public static class ThemesExtensions
 {
-    public static string GetName(this Themes theme)
+    public static string GetName(this Themes theme) => theme switch
     {
-        return theme switch
-        {
-            Themes.SYSTEM => "Synchronized with the operating system settings",
-            Themes.LIGHT => "Always use light theme",
-            Themes.DARK => "Always use dark theme",
-            
-            _ => "Unknown setting",
-        };
-    }
+        Themes.SYSTEM => "Synchronized with the operating system settings",
+        Themes.LIGHT => "Always use light theme",
+        Themes.DARK => "Always use dark theme",
+
+        _ => "Unknown setting",
+    };
 }
