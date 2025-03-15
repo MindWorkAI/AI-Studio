6f772e78b5f0b700474493c77e0ce54e25b8d97a
Thorsten Sommer <mail@tsommer.org>
2025-03-15 21:36:48 +0100

    Removed plugin state

--- app/MindWork AI Studio/Tools/PluginSystem/PluginState.cs
+++
@@ -1,8 +0,0 @@
-namespace AIStudio.Tools.PluginSystem;
-
-/// <summary>
-/// Represents the state of a plugin.
-/// </summary>
-/// <param name="Valid">True, when the plugin is valid.</param>
-/// <param name="Message">When the plugin is invalid, this contains the error message.</param>
-public readonly record struct PluginState(bool Valid, string Message);
