3f572075129e4ac87d237b306e553793e6f46fff
Thorsten Sommer <mail@tsommer.org>
2025-03-27 07:40:48 +0100

    Changed the plugin list to be a table

--- app/MindWork AI Studio/Pages/Plugins.razor.cs
+++ app/MindWork AI Studio/Pages/Plugins.razor.cs
@@ -1,7 +1,40 @@
+using AIStudio.Settings;
+using AIStudio.Tools.PluginSystem;
+
 using Microsoft.AspNetCore.Components;
 
 namespace AIStudio.Pages;
 
 public partial class Plugins : ComponentBase
 {
+    private const string GROUP_ENABLED = "Enabled";
+    private const string GROUP_DISABLED = "Disabled";
+    private const string GROUP_INTERNAL = "Internal";
+    
+    [Inject]
+    private SettingsManager SettingsManager { get; init; } = null!;
+    
+    private TableGroupDefinition<IPluginMetadata> groupConfig = null!;
+
+    #region Overrides of ComponentBase
+
+    protected override async Task OnInitializedAsync()
+    {
+        this.groupConfig = new TableGroupDefinition<IPluginMetadata>
+        {
+            Selector = pluginMeta =>
+            {
+                if (pluginMeta.IsInternal)
+                    return GROUP_INTERNAL;
+                
+                return SettingsManager.ConfigurationData.EnabledPlugins.Contains(pluginMeta.Id)
+                    ? GROUP_ENABLED
+                    : GROUP_DISABLED;
+            }
+        };
+        
+        await base.OnInitializedAsync();
+    }
+
+    #endregion
 }
