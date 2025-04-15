09c9834c04417850696f70ed99d698ed151f9055
krut_ni <nils.kruthoff@dlr.de>
2025-04-15 15:16:12 +0200

    WIP: Included a pandoc dialog to check for the availability and to include an automatic download process for the user

---
+++ app/MindWork AI Studio/Dialogs/PandocDialog.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AIStudio.Dialogs;
+
+public partial class PandocDialog : ComponentBase
+{
+    [CascadingParameter]
+    private IMudDialogInstance MudDialog { get; set; } = null!;
+
+    private bool isPandocAvailable;
+    private bool showSkeleton;
+    private bool showInstallPage;
+    
+
+    #region Overrides of ComponentBase
+    
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        this.showSkeleton = true;
+        await this.CheckPandocAvailabilityAsync();
+    }
+
+    #endregion
+    
+    private void Cancel() => this.MudDialog.Cancel();
+
+    private async Task CheckPandocAvailabilityAsync()
+    {
+        await Task.Delay(2500);
+        this.isPandocAvailable = await Pandoc.CheckAvailabilityAsync();
+        this.showSkeleton = false;
+        await this.InvokeAsync(this.StateHasChanged);
+    }
+
+    private void ProceedToInstallation() => this.showInstallPage = true;
+}
