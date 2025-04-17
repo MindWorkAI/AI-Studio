686739327792002cff00d700703c74c127c1e196
Thorsten Sommer <mail@tsommer.org>
2025-04-17 12:00:10 +0200

    Upgrade Supporters component to inherit MSGComponentBase.

--- app/MindWork AI Studio/Pages/Supporters.razor.cs
+++ app/MindWork AI Studio/Pages/Supporters.razor.cs
@@ -1,7 +1,14 @@
+using AIStudio.Components;
+
 using Microsoft.AspNetCore.Components;
 
 namespace AIStudio.Pages;
 
-public partial class Supporters : ComponentBase
+public partial class Supporters : MSGComponentBase
 {
+    #region Overrides of MSGComponentBase
+
+    public override string ComponentName => nameof(Supporters);
+
+    #endregion
 }
