bf4732916601db4470bc088792456393dc876b2d
Thorsten Sommer <mail@tsommer.org>
2025-04-25 15:29:17 +0200

    Add language plugin selection and loading feedback in I18N assistant

--- app/MindWork AI Studio/Assistants/I18N/AssistantI18N.razor.cs
+++ app/MindWork AI Studio/Assistants/I18N/AssistantI18N.razor.cs
@@ -1,4 +1,7 @@
 using AIStudio.Dialogs.Settings;
+using AIStudio.Tools.PluginSystem;
+
+using Microsoft.Extensions.FileProviders;
 
 namespace AIStudio.Assistants.I18N;
 
@@ -25,11 +28,14 @@ public partial class AssistantI18N : AssistantBaseCore<SettingsDialogI18N>
     protected override string SubmitText => "Localize AI Studio";
     
     protected override Func<Task> SubmitAction => this.LocalizeText;
+
+    protected override bool SubmitDisabled => !this.localizationPossible;
     
     protected override void ResetForm()
     {
         if (!this.MightPreselectValues())
         {
+            this.selectedLanguagePluginId = InternalPlugin.LANGUAGE_EN_US.MetaData().Id;
             this.selectedTargetLanguage = CommonLanguages.AS_IS;
             this.customTargetLanguage = string.Empty;
         }
@@ -39,6 +45,7 @@ public partial class AssistantI18N : AssistantBaseCore<SettingsDialogI18N>
     {
         if (this.SettingsManager.ConfigurationData.I18N.PreselectOptions)
         {
+            this.selectedLanguagePluginId = this.SettingsManager.ConfigurationData.I18N.PreselectedLanguagePluginId;
             this.selectedTargetLanguage = this.SettingsManager.ConfigurationData.I18N.PreselectedTargetLanguage;
             this.customTargetLanguage = this.SettingsManager.ConfigurationData.I18N.PreselectOtherLanguage;
             return true;
@@ -49,6 +56,105 @@ public partial class AssistantI18N : AssistantBaseCore<SettingsDialogI18N>
     
     private CommonLanguages selectedTargetLanguage;
     private string customTargetLanguage = string.Empty;
+    private bool isLoading = true;
+    private string loadingIssue = string.Empty;
+    private bool localizationPossible;
+    private string searchString = string.Empty;
+    private Guid selectedLanguagePluginId;
+    private Dictionary<string, string> addedKeys = [];
+    private Dictionary<string, string> removedKeys = [];
+
+    #region Overrides of AssistantBase<SettingsDialogI18N>
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        await this.LoadData();
+    }
+
+    #endregion
+
+    private async Task OnLanguagePluginChanged(Guid pluginId)
+    {
+        this.selectedLanguagePluginId = pluginId;
+        await this.LoadData();
+        this.StateHasChanged();
+    }
+    
+    private async Task LoadData()
+    {
+        this.isLoading = true;
+        this.StateHasChanged();
+        
+        //
+        // Read the file `Assistants\I18N\allTexts.lua`:
+        //
+        
+        #if DEBUG
+        var filePath = Path.Join(Environment.CurrentDirectory, "Assistants", "I18N");
+        var resourceFileProvider = new PhysicalFileProvider(filePath);
+        #else
+        var resourceFileProvider = new ManifestEmbeddedFileProvider(Assembly.GetAssembly(type: typeof(Program))!, "Assistants.I18N");
+        #endif
+
+        var file = resourceFileProvider.GetFileInfo("allTexts.lua");
+        await using var fileStream = file.CreateReadStream();
+        using var reader = new StreamReader(fileStream);
+        var newI18NDataLuaCode = await reader.ReadToEndAsync();
+        
+        //
+        // Next, we try to load the text as a language plugin -- without
+        // actually starting the plugin:
+        //
+        var newI18NPlugin = await PluginFactory.Load(null, newI18NDataLuaCode);
+        switch (newI18NPlugin)
+        {
+            case NoPlugin noPlugin when noPlugin.Issues.Any():
+                this.loadingIssue = noPlugin.Issues.First();
+                break;
+            
+            case NoPlugin:
+                this.loadingIssue = "Was not able to load the I18N plugin. Please check the plugin code.";
+                break;
+            
+            case { IsValid: false } plugin when plugin.Issues.Any():
+                this.loadingIssue = plugin.Issues.First();
+                break;
+            
+            case PluginLanguage pluginLanguage:
+                this.loadingIssue = string.Empty;
+                var newI18NContent = pluginLanguage.Content;
+
+                if(PluginFactory.RunningPlugins.FirstOrDefault(n => n is PluginLanguage && n.Id == this.selectedLanguagePluginId) is not PluginLanguage comparisonPlugin)
+                {
+                    this.loadingIssue = $"Was not able to load the language plugin for comparison ({this.selectedLanguagePluginId}). Please select a valid, loaded & running language plugin.";
+                    break;
+                }
+                
+                var currentI18NContent = comparisonPlugin.Content;
+                this.addedKeys = newI18NContent.ExceptBy(currentI18NContent.Keys, n => n.Key).ToDictionary();
+                this.removedKeys = currentI18NContent.ExceptBy(newI18NContent.Keys, n => n.Key).ToDictionary();
+                this.localizationPossible = true;
+                break;
+        }
+        
+        this.isLoading = false;
+        this.StateHasChanged();
+    }
+    
+    private bool FilterFunc(KeyValuePair<string, string> element)
+    {
+        if (string.IsNullOrWhiteSpace(this.searchString))
+            return true;
+        
+        if (element.Key.Contains(this.searchString, StringComparison.OrdinalIgnoreCase))
+            return true;
+        
+        if (element.Value.Contains(this.searchString, StringComparison.OrdinalIgnoreCase))
+            return true;
+        
+        return false;
+    }
 
     private string? ValidatingTargetLanguage(CommonLanguages language)
     {
