53ec4c2162c37a891c43c64105b87a745b3aad84
Thorsten Sommer <SommerEngineering@users.noreply.github.com>
2025-05-02 11:50:14 +0200

    Improved app settings to apply language changes immediately (#437)

--- app/MindWork AI Studio/Components/Settings/SettingsPanelApp.razor.cs
+++ app/MindWork AI Studio/Components/Settings/SettingsPanelApp.razor.cs
@@ -9,4 +9,16 @@ public partial class SettingsPanelApp : SettingsPanelBase
         this.SettingsManager.ConfigurationData.App.PreviewVisibility = previewVisibility;
         this.SettingsManager.ConfigurationData.App.EnabledPreviewFeatures = previewVisibility.FilterPreviewFeatures(this.SettingsManager.ConfigurationData.App.EnabledPreviewFeatures);
     }
+
+    private async Task UpdateLangBehaviour(LangBehavior behavior)
+    {
+        this.SettingsManager.ConfigurationData.App.LanguageBehavior = behavior;
+        await this.MessageBus.SendMessage<bool>(this, Event.PLUGINS_RELOADED);
+    }
+
+    private async Task UpdateManuallySelectedLanguage(Guid pluginId)
+    {
+        this.SettingsManager.ConfigurationData.App.LanguagePluginId = pluginId;
+        await this.MessageBus.SendMessage<bool>(this, Event.PLUGINS_RELOADED);
+    }
 }
