bf04cb12b161eea7066ba5375df0519932571851
Thorsten Sommer <mail@tsommer.org>
2025-05-03 14:25:39 +0200

    Refactor metadata initialization

--- runtime/src/metadata.rs
+++ runtime/src/metadata.rs
@@ -1,5 +1,5 @@
-use std::sync::Mutex;
 use once_cell::sync::Lazy;
+use std::sync::Mutex;
 
 pub static META_DATA: Lazy<Mutex<Option<MetaData>>> = Lazy::new(|| Mutex::new(None));
 
@@ -15,4 +15,46 @@ pub struct MetaData {
     pub tauri_version: String,
     pub app_commit_hash: String,
     pub architecture: String,
+    pub pdfium_version: String,
+}
+
+impl MetaData {
+    /// Creates a new instance of `MetaData` from the given metadata string.
+    pub fn init_from_string(metadata: &str) -> Self {
+        
+        // When the metadata is already initialized, return the existing instance:
+        if let Some(metadata) = META_DATA.lock().unwrap().as_ref() {
+            return metadata.clone();
+        }
+        
+        let mut metadata_lines = metadata.lines();
+        let app_version = metadata_lines.next().unwrap();
+        let build_time = metadata_lines.next().unwrap();
+        let build_number = metadata_lines.next().unwrap();
+        let dotnet_sdk_version = metadata_lines.next().unwrap();
+        let dotnet_version = metadata_lines.next().unwrap();
+        let rust_version = metadata_lines.next().unwrap();
+        let mud_blazor_version = metadata_lines.next().unwrap();
+        let tauri_version = metadata_lines.next().unwrap();
+        let app_commit_hash = metadata_lines.next().unwrap();
+        let architecture = metadata_lines.next().unwrap();
+        let pdfium_version = metadata_lines.next().unwrap();
+
+        let metadata = MetaData {
+            architecture: architecture.to_string(),
+            app_commit_hash: app_commit_hash.to_string(),
+            app_version: app_version.to_string(),
+            build_number: build_number.to_string(),
+            build_time: build_time.to_string(),
+            dotnet_sdk_version: dotnet_sdk_version.to_string(),
+            dotnet_version: dotnet_version.to_string(),
+            mud_blazor_version: mud_blazor_version.to_string(),
+            rust_version: rust_version.to_string(),
+            tauri_version: tauri_version.to_string(),
+            pdfium_version: pdfium_version.to_string(),
+        };
+
+        *META_DATA.lock().unwrap() = Some(metadata.clone());
+        metadata
+    }
 }
