9fa0f95611a774268636ee02613d4cdd6bc232a3
Thorsten Sommer <SommerEngineering@users.noreply.github.com>
2025-05-11 16:57:52 +0200

    Localized the embedding configuration (#455)

--- app/MindWork AI Studio/Assistants/BiasDay/BiasOfTheDayAssistant.razor.cs
+++ app/MindWork AI Studio/Assistants/BiasDay/BiasOfTheDayAssistant.razor.cs
@@ -12,16 +12,7 @@ public partial class BiasOfTheDayAssistant : AssistantBaseCore<SettingsDialogAss
     
     protected override string Title => T("Bias of the Day");
     
-    protected override string Description =>
-        T("""
-          Learn about a different cognitive bias every day. You can also ask the LLM your questions. The idea behind
-          "Bias of the Day" is based on work by Buster Benson, John Manoogian III, and Brian Rene Morrissette. Buster
-          Benson grouped the biases, and the original texts come from Wikipedia. Brian Rene Morrissette condensed them
-          into a shorter version. Finally, John Manoogian III created the original poster based on Benson's work and
-          Morrissette's texts. Thorsten Sommer compared all texts for integration into AI Studio with the current Wikipedia
-          versions, updated them, and added source references. The idea of learning about one bias each day based on John's
-          poster comes from Drew Nelson.
-          """);
+    protected override string Description => T("""Learn about a different cognitive bias every day. You can also ask the LLM your questions. The idea behind  "Bias of the Day" is based on work by Buster Benson, John Manoogian III, and Brian Rene Morrissette. Buster Benson grouped the biases, and the original texts come from Wikipedia. Brian Rene Morrissette condensed them into a shorter version. Finally, John Manoogian III created the original poster based on Benson's work and Morrissette's texts. Thorsten Sommer compared all texts for integration into AI Studio with the current Wikipedia versions, updated them, and added source references. The idea of learning about one bias each day based on John's poster comes from Drew Nelson.""");
 
     protected override string SystemPrompt => $"""
                                               You are a friendly, helpful expert on cognitive bias. You studied psychology and
