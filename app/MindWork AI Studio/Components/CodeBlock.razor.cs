d9fb0dedafe8eed0690ca1786bfd22df7ad63bcd
nilsk <nils.kruthoff@iubh-fernstudium.de>
2025-05-19 10:12:42 +0200

    Introduced a flexible code block that enables to show code examples in a tab menu, single blocks or inline code blocks

---
+++ app/MindWork AI Studio/Components/CodeBlock.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+
+namespace AIStudio.Components;
+
+public partial class CodeBlock : ComponentBase
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+    
+    [Parameter] 
+    public string? Title { get; set; } = string.Empty;
+    
+    [Parameter] 
+    public bool IsInline { get; set; } = false;
+    
+    [CascadingParameter]
+    public CodeTabs? ParentTabs { get; set; }
+    
+    private static readonly string DARK_BACKGROUND_COLOR = "#2d2d2d";
+    private static readonly string DARK_FOREGROUND_COLOR = "#f8f8f2";
+
+    protected override void OnInitialized()
+    {
+        if (this.ParentTabs is not null && this.Title is not null)
+        {
+            RenderFragment blockSelf = builder =>
+            {
+                builder.OpenComponent<CodeBlock>(0);
+                builder.AddAttribute(1, "Title", this.Title);
+                builder.AddAttribute(2, "ChildContent", this.ChildContent);
+                builder.CloseComponent();
+            };
+            this.ParentTabs.RegisterBlock(this.Title, blockSelf);
+        }
+    }
+
+    private string BlockPadding()
+    {
+        return this.ParentTabs is null ? "padding: 16px !important;" : "padding: 8px !important";
+    }
+}
---
+++ app/MindWork AI Studio/Components/CodeTabs.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AIStudio.Components;
+
+public partial class CodeTabs : ComponentBase
+{
+    [Parameter] 
+    public RenderFragment? ChildContent { get; set; }
+    
+    private List<CodeTabItem> blocks = new();
+    private int selectedIndex = 0;
+
+    internal void RegisterBlock(string title, RenderFragment fragment)
+    {
+        this.blocks.Add(new CodeTabItem
+        {
+            Title = title,
+            Fragment = fragment,
+        });
+        this.StateHasChanged();
+    }
+
+    private class CodeTabItem
+    {
+        public string Title { get; init; } = string.Empty;
+        public RenderFragment Fragment { get; init; }
+    }
+}
