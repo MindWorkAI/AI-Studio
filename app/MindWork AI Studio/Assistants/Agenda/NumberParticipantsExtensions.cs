e3c272b0c4ae1a1bb291ebd75b7d4dd8590e4974
Thorsten Sommer <SommerEngineering@users.noreply.github.com>
2025-05-26 20:50:24 +0200

    Added more text content to the I18N system (#485)

--- app/MindWork AI Studio/Assistants/Agenda/NumberParticipantsExtensions.cs
+++ app/MindWork AI Studio/Assistants/Agenda/NumberParticipantsExtensions.cs
@@ -2,22 +2,24 @@ namespace AIStudio.Assistants.Agenda;
 
 public static class NumberParticipantsExtensions
 {
+    private static string TB(string fallbackEN) => Tools.PluginSystem.I18N.I.T(fallbackEN, typeof(NumberParticipantsExtensions).Namespace, nameof(NumberParticipantsExtensions));
+    
     public static string Name(this NumberParticipants numberParticipants) => numberParticipants switch
     {
-        NumberParticipants.NOT_SPECIFIED => "Please select how many participants are expected",
+        NumberParticipants.NOT_SPECIFIED => TB("Please select how many participants are expected"),
         
-        NumberParticipants.PEER_TO_PEER => "2 (peer to peer)",
+        NumberParticipants.PEER_TO_PEER => TB("2 (peer to peer)"),
         
-        NumberParticipants.SMALL_GROUP => "3 - 5 (small group)",
-        NumberParticipants.LARGE_GROUP => "6 - 12 (large group)",
-        NumberParticipants.MULTIPLE_SMALL_GROUPS => "13 - 20 (multiple small groups)",
-        NumberParticipants.MULTIPLE_LARGE_GROUPS => "21 - 30 (multiple large groups)",
+        NumberParticipants.SMALL_GROUP => TB("3 - 5 (small group)"),
+        NumberParticipants.LARGE_GROUP => TB("6 - 12 (large group)"),
+        NumberParticipants.MULTIPLE_SMALL_GROUPS => TB("13 - 20 (multiple small groups)"),
+        NumberParticipants.MULTIPLE_LARGE_GROUPS => TB("21 - 30 (multiple large groups)"),
         
-        NumberParticipants.SYMPOSIUM => "31 - 100 (symposium)",
-        NumberParticipants.CONFERENCE => "101 - 200 (conference)",
-        NumberParticipants.CONGRESS => "201 - 1,000 (congress)",
+        NumberParticipants.SYMPOSIUM => TB("31 - 100 (symposium)"),
+        NumberParticipants.CONFERENCE => TB("101 - 200 (conference)"),
+        NumberParticipants.CONGRESS => TB("201 - 1,000 (congress)"),
         
-        NumberParticipants.LARGE_EVENT => "1,000+ (large event)",
+        NumberParticipants.LARGE_EVENT => TB("1,000+ (large event)"),
         
         _ => "Unknown"
     };
