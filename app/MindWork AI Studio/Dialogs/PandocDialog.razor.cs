cecf777827cc5e877b5100077dd052e91097f680
krut_ni <nils.kruthoff@dlr.de>
2025-05-26 16:43:36 +0200

    WIP: Adjusted styling for user experience and added installation with installers; Added async loading of license text

--- app/MindWork AI Studio/Dialogs/PandocDialog.razor.cs
+++ app/MindWork AI Studio/Dialogs/PandocDialog.razor.cs
@@ -4,14 +4,20 @@ namespace AIStudio.Dialogs;
 
 public partial class PandocDialog : ComponentBase
 {
+    [Inject]
+    private HttpClient HttpClient { get; set; } = null!;
+    
     [CascadingParameter]
     private IMudDialogInstance MudDialog { get; set; } = null!;
+    
+    private static readonly string LICENCE_URI = "https://raw.githubusercontent.com/jgm/pandoc/master/COPYRIGHT";
 
     private bool isPandocAvailable;
     private bool showSkeleton;
     private bool showInstallPage;
+    private string? licenseText;
+    private bool isLoading;
     
-
     #region Overrides of ComponentBase
     
     protected override async Task OnInitializedAsync()
@@ -33,4 +39,40 @@ public partial class PandocDialog : ComponentBase
     }
 
     private void ProceedToInstallation() => this.showInstallPage = true;
+    
+    private async Task OnExpandedChanged(bool newVal)
+    {
+        if (newVal)
+        {
+            this.isLoading = true;
+            try
+            {
+                await Task.Delay(600);
+
+                this.licenseText = await this.LoadLicenseTextAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler beim Laden des Lizenztexts: {ex.Message}");
+                this.licenseText = "Fehler beim Laden des Lizenztexts.";
+            }
+            finally
+            {
+                this.isLoading = false;
+            }
+        }
+        else
+        {
+            await Task.Delay(350);
+            this.licenseText = string.Empty;
+        }
+    }
+    
+    private async Task<string> LoadLicenseTextAsync()
+    {
+        var response = await this.HttpClient.GetAsync(LICENCE_URI);
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        return content;
+    }
 }
