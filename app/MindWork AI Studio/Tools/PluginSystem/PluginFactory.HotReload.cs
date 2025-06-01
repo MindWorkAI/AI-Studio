85fcb4a6c862165677982268f88180024ac0f701
Thorsten Sommer <mail@tsommer.org>
2025-06-01 20:54:25 +0200

    Refactor hot reload system to improve event handling & monitor delete events as well

--- app/MindWork AI Studio/Tools/PluginSystem/PluginFactory.HotReload.cs
+++ app/MindWork AI Studio/Tools/PluginSystem/PluginFactory.HotReload.cs
@@ -15,32 +15,11 @@ public static partial class PluginFactory
         LOG.LogInformation($"Start hot reloading plugins for path '{HOT_RELOAD_WATCHER.Path}'.");
         try
         {
-            var messageBus = Program.SERVICE_PROVIDER.GetRequiredService<MessageBus>();
-        
             HOT_RELOAD_WATCHER.IncludeSubdirectories = true;
             HOT_RELOAD_WATCHER.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName;
             HOT_RELOAD_WATCHER.Filter = "*.lua";
-            HOT_RELOAD_WATCHER.Changed += async (_, args) =>
-            {
-                var changeType = args.ChangeType.ToString().ToLowerInvariant();
-                if (!await HOT_RELOAD_SEMAPHORE.WaitAsync(0))
-                {
-                    LOG.LogInformation($"File changed ({changeType}): {args.FullPath}. Already processing another change.");
-                    return;
-                }
-
-                try
-                {
-                    LOG.LogInformation($"File changed ({changeType}): {args.FullPath}. Reloading plugins...");
-                    await LoadAll();
-                    await messageBus.SendMessage<bool>(null, Event.PLUGINS_RELOADED);
-                }
-                finally
-                {
-                    HOT_RELOAD_SEMAPHORE.Release();
-                }
-            };
-            
+            HOT_RELOAD_WATCHER.Changed += HotReloadEventHandler;
+            HOT_RELOAD_WATCHER.Deleted += HotReloadEventHandler;
             HOT_RELOAD_WATCHER.EnableRaisingEvents = true;
         }
         catch (Exception e)
@@ -50,6 +29,34 @@ public static partial class PluginFactory
         finally
         {
             LOG.LogInformation("Hot reloading plugins set up.");
+        }
+    }
+    
+    private static async void HotReloadEventHandler(object _, FileSystemEventArgs args)
+    {
+        try
+        {
+            var changeType = args.ChangeType.ToString().ToLowerInvariant();
+            if (!await HOT_RELOAD_SEMAPHORE.WaitAsync(0))
+            {
+                LOG.LogInformation($"File changed ({changeType}): {args.FullPath}. Already processing another change.");
+                return;
+            }
+
+            try
+            {
+                LOG.LogInformation($"File changed ({changeType}): {args.FullPath}. Reloading plugins...");
+                await LoadAll();
+                await MessageBus.INSTANCE.SendMessage<bool>(null, Event.PLUGINS_RELOADED);
+            }
+            finally
+            {
+                HOT_RELOAD_SEMAPHORE.Release();
+            }
+        }
+        catch (Exception e)
+        {
+            LOG.LogError(e, $"Error while handling hot reload event for file '{args.FullPath}' with change type '{args.ChangeType}'.");
         }
     }
 }
