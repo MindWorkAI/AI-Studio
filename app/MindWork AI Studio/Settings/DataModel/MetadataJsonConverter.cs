2ce863ebd6c793665db133ba3ca731890907bae8
krut_ni <nils.kruthoff@dlr.de>
2025-06-23 14:14:35 +0200

    implemented a json converter to parse metadata

---
+++ app/MindWork AI Studio/Settings/DataModel/MetadataJsonConverter.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AIStudio.Settings.DataModel;
+
+public class MetadataJsonConverter : JsonConverter<Metadata>
+{
+    public override Metadata? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        
+        using (var jsonDoc = JsonDocument.ParseValue(ref reader))
+        {
+            var root = jsonDoc.RootElement;
+
+            if (root.TryGetProperty("Text", out _))
+            {
+                return JsonSerializer.Deserialize<TextMetadata>(root.GetRawText(), options);
+            }
+            else if (root.TryGetProperty("Pdf", out _))
+            {
+                return JsonSerializer.Deserialize<PdfMetadata>(root.GetRawText(), options);
+            }
+            else if (root.TryGetProperty("Spreadsheet", out _))
+            {
+                return JsonSerializer.Deserialize<SpreadsheetMetadata>(root.GetRawText(), options);
+            }
+            else if (root.TryGetProperty("Presentation", out _))
+            {
+                return JsonSerializer.Deserialize<PresentationMetadata>(root.GetRawText(), options);
+            }
+            else if (root.TryGetProperty("Image", out _))
+            {
+                return JsonSerializer.Deserialize<ImageMetadata>(root.GetRawText(), options);
+            }
+            else if (root.TryGetProperty("Document", out _))
+            {
+                return JsonSerializer.Deserialize<DocumentMetadata>(root.GetRawText(), options);
+            }
+            else
+            {
+                // Unbekannter Typ
+                return null;
+            }
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, Metadata value, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}
