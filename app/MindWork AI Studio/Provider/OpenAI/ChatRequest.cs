c4953b6ac9e7be0ceb95b6a6a2c3c369aa254137
Thorsten Sommer <mail@tsommer.org>
2025-08-31 17:22:12 +0200

    Removed deprecated seed & unused frequency penalty parameters

--- app/MindWork AI Studio/Provider/OpenAI/ChatRequest.cs
+++ app/MindWork AI Studio/Provider/OpenAI/ChatRequest.cs
@@ -8,14 +8,8 @@ namespace AIStudio.Provider.OpenAI;
 /// <param name="Model">Which model to use for chat completion.</param>
 /// <param name="Messages">The chat messages.</param>
 /// <param name="Stream">Whether to stream the chat completion.</param>
-/// <param name="Seed">The seed for the chat completion.</param>
-/// <param name="FrequencyPenalty">The frequency penalty for the chat completion.</param>
 public readonly record struct ChatRequest(
     string Model,
     IList<Message> Messages,
-    bool Stream,
-    int Seed,
-    
-    [Range(-2.0f, 2.0f)]
-    float FrequencyPenalty
+    bool Stream
 );
