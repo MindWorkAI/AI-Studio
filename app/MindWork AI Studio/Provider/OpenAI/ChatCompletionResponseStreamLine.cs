9d31ce62c3a6c677cdd2e32519812a4b5c4656ea
Thorsten Sommer <mail@tsommer.org>
2025-09-02 22:14:43 +0200

    Refactored chat completion data records

--- app/MindWork AI Studio/Provider/OpenAI/ChatCompletionResponseStreamLine.cs
+++ app/MindWork AI Studio/Provider/OpenAI/ChatCompletionResponseStreamLine.cs
@@ -9,7 +9,7 @@ namespace AIStudio.Provider.OpenAI;
 /// <param name="Model">The model used for the response.</param>
 /// <param name="SystemFingerprint">The system fingerprint; together with the seed, this allows you to reproduce the response.</param>
 /// <param name="Choices">The choices made by the AI.</param>
-public record ChatCompletionResponseStreamLine(string Id, string Object, uint Created, string Model, string SystemFingerprint, IList<Choice> Choices) : IResponseStreamLine
+public record ChatCompletionResponseStreamLine(string Id, string Object, uint Created, string Model, string SystemFingerprint, IList<ChatCompletionChoice> Choices) : IResponseStreamLine
 {
     public ChatCompletionResponseStreamLine() : this(string.Empty, string.Empty, 0, string.Empty, string.Empty, [])
     {
@@ -20,27 +20,14 @@ public record ChatCompletionResponseStreamLine(string Id, string Object, uint Cr
 
     /// <inheritdoc />
     public ContentStreamChunk GetContent() => new(this.Choices[0].Delta.Content, []);
-}
 
-/// <summary>
-/// Data model for a choice made by the AI.
-/// </summary>
-/// <param name="Index">The index of the choice.</param>
-/// <param name="Delta">The delta text of the choice.</param>
-public record Choice(int Index, Delta Delta)
-{
-    public Choice() : this(0, new (string.Empty))
-    {
-    }
-}
+    #region Implementation of IAnnotationStreamLine
 
-/// <summary>
-/// The delta text of a choice.
-/// </summary>
-/// <param name="Content">The content of the delta text.</param>
-public record Delta(string Content)
-{
-    public Delta() : this(string.Empty)
-    {
-    }
+    /// <inheritdoc />
+    public bool ContainsSources() => false;
+
+    /// <inheritdoc />
+    public IList<ISource> GetSources() => [];
+
+    #endregion
 }
