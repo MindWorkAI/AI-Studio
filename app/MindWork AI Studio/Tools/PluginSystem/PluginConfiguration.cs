a6519ca0e246ae86907b6d429de110106bc0038e
Peer Schütt <peerschuett1996@gmail.com>
2025-12-04 15:37:13 +0100

    Fixed a bug in GUID comparisons for configuration plugins (#578)

--- app/MindWork AI Studio/Tools/PluginSystem/PluginConfiguration.cs
+++ app/MindWork AI Studio/Tools/PluginSystem/PluginConfiguration.cs
@@ -77,7 +77,7 @@ public sealed class PluginConfiguration(bool isInternal, LuaState state, PluginT
         PluginConfigurationObject.TryParse(PluginConfigurationObjectType.PROFILE, x => x.Profiles, x => x.NextProfileNum, mainTable, this.Id, ref this.configObjects, dryRun);
         
         // Config: preselected profile?
-        ManagedConfiguration.TryProcessConfiguration(x => x.App, x => x.PreselectedProfile, this.Id, settingsTable, dryRun);
+        ManagedConfiguration.TryProcessConfiguration(x => x.App, x => x.PreselectedProfile, Guid.Empty, this.Id, settingsTable, dryRun);
         
         message = string.Empty;
         return true;
