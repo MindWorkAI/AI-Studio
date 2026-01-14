42d538d2d8881b977ba223531ba32e64b294aad4
Thorsten Sommer <mail@tsommer.org>
2026-01-14 09:02:14 +0100

    Removed not used documentation

--- app/MindWork AI Studio/Tools/Rust/QdrantInfo.cs
+++ app/MindWork AI Studio/Tools/Rust/QdrantInfo.cs
@@ -3,8 +3,6 @@
 /// <summary>
 /// The response of the Qdrant information request.
 /// </summary>
-/// <param name="portHTTP">The port number for HTTP communication with Qdrant.</param>
-/// <param name="portGRPC">The port number for gRPC communication with Qdrant</param>
 public record struct QdrantInfo
 {
     public string Path { get; init; }
