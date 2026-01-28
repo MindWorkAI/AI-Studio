dac25c51c68bc9a1db10c7a8dc46498d722d8873
Thorsten Sommer <SommerEngineering@users.noreply.github.com>
2026-01-28 09:45:41 +0100

    Added NoSettingsPanel option for assistants

--- app/MindWork AI Studio/Assistants/AssistantBase.razor.cs
+++ app/MindWork AI Studio/Assistants/AssistantBase.razor.cs
@@ -1,6 +1,7 @@
 using AIStudio.Chat;
 using AIStudio.Provider;
 using AIStudio.Settings;
+using AIStudio.Dialogs.Settings;
 using AIStudio.Tools.Services;
 
 using Microsoft.AspNetCore.Components;
@@ -81,6 +82,8 @@ public abstract partial class AssistantBase<TSettings> : AssistantLowerBase wher
     protected virtual ChatThread ConvertToChatThread => this.chatThread ?? new();
 
     protected virtual IReadOnlyList<IButtonData> FooterButtons => [];
+
+    protected virtual bool HasSettingsPanel => typeof(TSettings) != typeof(NoSettingsPanel);
     
     protected AIStudio.Settings.Provider providerSettings = Settings.Provider.NONE;
     protected MudForm? form;
@@ -310,6 +313,9 @@ public abstract partial class AssistantBase<TSettings> : AssistantLowerBase wher
     
     protected async Task OpenSettingsDialog()
     {
+        if (!this.HasSettingsPanel)
+            return;
+
         var dialogParameters = new DialogParameters();
         await this.DialogService.ShowAsync<TSettings>(null, dialogParameters, DialogOptions.FULLSCREEN);
     }
