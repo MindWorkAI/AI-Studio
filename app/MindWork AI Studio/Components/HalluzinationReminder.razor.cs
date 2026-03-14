078d48ca6fd17227ced21bbd402fd89e204970d5
Peer Schütt <peerschuett1996@gmail.com>
2026-03-14 10:54:52 +0100

    Added a LLM halluzination reminder (#694)
    Co-authored-by: Thorsten Sommer <SommerEngineering@users.noreply.github.com>

---
+++ app/MindWork AI Studio/Components/HalluzinationReminder.razor.cs
@@ -0,0 +1,15 @@
+﻿using AIStudio.Tools.PluginSystem;
+using Microsoft.AspNetCore.Components;
+
+namespace AIStudio.Components;
+
+public partial class HalluzinationReminder: ComponentBase
+{
+    private static string TB(string fallbackEN) => I18N.I.T(fallbackEN, typeof(HalluzinationReminder).Namespace, nameof(HalluzinationReminder));
+    
+    [Parameter]
+    public string Text { get; set; } = TB("LLMs can make mistakes. Check important information.");
+
+    [Parameter]
+    public string ContainerClass { get; set; } = "mt-2 mb-1";
+}
