09d13e66c27b47c5436affdb8a1750bc1b95831b
nilsk <nils.kruthoff@iubh-fernstudium.de>
2026-03-31 15:36:22 +0200

    integrating the security rules and badge to the assistant page and block

--- app/MindWork AI Studio/Pages/Assistants.razor.cs
+++ app/MindWork AI Studio/Pages/Assistants.razor.cs
@@ -1,15 +1,28 @@
 using AIStudio.Components;
 using AIStudio.Tools.PluginSystem;
 using AIStudio.Tools.PluginSystem.Assistants;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.AspNetCore.Components;
 
 namespace AIStudio.Pages;
 
 public partial class Assistants : MSGComponentBase
 {
+    protected override async Task OnInitializedAsync()
+    {
+        this.ApplyFilters([], [ Event.CONFIGURATION_CHANGED, Event.PLUGINS_RELOADED ]);
+        await base.OnInitializedAsync();
+    }
+
     private IReadOnlyCollection<PluginAssistants> AssistantPlugins =>
         PluginFactory.RunningPlugins.OfType<PluginAssistants>()
             .Where(plugin => this.SettingsManager.IsPluginEnabled(plugin))
             .ToList();
+
+    protected override Task ProcessIncomingMessage<T>(ComponentBase? sendingComponent, Event triggeredEvent, T? data) where T : default
+    {
+        if (triggeredEvent is Event.CONFIGURATION_CHANGED or Event.PLUGINS_RELOADED)
+            return this.InvokeAsync(this.StateHasChanged);
+
+        return Task.CompletedTask;
+    }
 }
