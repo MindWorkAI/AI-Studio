331b8f3764645102ff83bb5dd6ebcb196558e4d1
Thorsten Sommer <SommerEngineering@users.noreply.github.com>
2026-07-06 20:40:58 +0200

    Prepared release v26.7.2 (#841)

--- app/MindWork AI Studio/Components/Changelog.Logs.cs
+++ app/MindWork AI Studio/Components/Changelog.Logs.cs
@@ -13,6 +13,7 @@ public partial class Changelog
     
     public static readonly Log[] LOGS = 
     [
+        new (244, "v26.7.2, build 244 (2026-07-06 18:35 UTC)", "v26.7.2.md"),
         new (243, "v26.7.1, build 243 (2026-07-05 16:39 UTC)", "v26.7.1.md"),
         new (242, "v26.6.2, build 242 (2026-06-21 14:07 UTC)", "v26.6.2.md"),
         new (241, "v26.6.1, build 241 (2026-06-11 13:49 UTC)", "v26.6.1.md"),
